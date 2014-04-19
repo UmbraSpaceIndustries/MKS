ae1b54d384d5bf76784125fc77da8ed2869044f0
Bob Palmer <rbpalmer2222@gmail.com>
2014-04-19 00:39:46 -0400

    ProxyLogistics and new Docking Modules

--- Source/KolonyTools/KolonyConverter.cs
+++ Source/KolonyTools/KolonyConverter.cs
@@ -9,12 +9,6 @@ namespace KolonyTools
     public class KolonyConverter : TacGenericConverter
     {
         [KSPField]
-        public int LogisticsRange = 100;
-
-        [KSPField]
-        public bool LogisticsEnabled = true;
-
-        [KSPField]
         public string RequiredResources = "";
         
         [KSPField]
@@ -22,9 +16,7 @@ namespace KolonyTools
 
         [KSPField]
         public bool AllowsEfficiency = true;
-        
-        [KSPField(guiActive = true, guiName = "Workspaces")]
-        public string efficiencyStats = "Unknown";
+
         [KSPField(guiActive = true, guiName = "Efficiency")]
         public string efficiency = "Unknown";
 
@@ -65,10 +57,7 @@ namespace KolonyTools
                     converterStatus = "Missing " + missingFixedResources;
                     return;
                 }
-                if(LogisticsEnabled && converterEnabled) GetMissingResources();
                 base.OnFixedUpdate();
-                if (LogisticsEnabled && converterEnabled) StoreExcessResources();
-
                 conversionRate = baseConversionRate;
             }
             catch (Exception ex)
@@ -78,233 +67,9 @@ namespace KolonyTools
             }
         }
 
-        private void GetMissingResources()
-        {
-            try
-            {
-                //Only works when landed
-                if (!vessel.Landed) return;
-                foreach (var r in inputResourceList)
-                {
-                    var rName = r.resource.name;
-                    var curAmount = part.Resources[rName].amount;
-                    var maxAmount = part.Resources[rName].maxAmount;
-                    var storageSpace = maxAmount - curAmount;
-                    var needAmount = (r.ratio * 2) - curAmount;
-                    if (needAmount > storageSpace) needAmount = storageSpace;
-                    needAmount = Math.Ceiling(needAmount); //Get a bit extra for good measure
-                    if (needAmount > 0)
-                    {
-                        TransferResources(r.resource, needAmount, TransferType.TakeResources);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                print(String.Format("[MKS] - ERROR in GetMissingResources - {0}",ex.Message));
-                throw;
-            }
-        }
 
-        private void StoreExcessResources()
-        {
-            try
-            { 
-                //Only works when landed
-                if (!vessel.Landed) return;
-                foreach (var r in outputResourceList)
-                {
-                    var rName = r.resource.name;
-                    var curAmount = part.Resources[rName].amount;
-                    var maxAmount = part.Resources[rName].maxAmount;
 
-                    //490+(20*2)-500 = 30  [store]
-                    //450+(20*2)-500 = -10 [nostore]
-                    var storeAmount = curAmount + (r.ratio * 2) - maxAmount;
-                    storeAmount = Math.Floor(storeAmount) -1; //Leave a bit inside
-                    if (storeAmount > 0)
-                    {
-                        //Start by moving stuff to crewed areas if appropriate.  
-                        if (LSResources.Contains(rName))
-                        {
-                            storeAmount = TransferLifeSupport(rName, storeAmount);
-                        }
-                        //Any excess is handled normally.
-                        TransferResources(r.resource, storeAmount, TransferType.StoreResources);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                print(String.Format("[MKS] - ERROR in StoreExcessResources - {0}",ex.Message));
-                throw;
-            }
 
-        }
-
-        private double TransferLifeSupport(string name, double amount)
-        {
-            try
-            { 
-                //Start finding crewed modules.  Keep adding life support until our amount is depleted.
-                var nearVessels = GetNearbyVessels();
-                var transferAmount = amount;
-
-                foreach (var v in nearVessels)
-                {
-                    if (transferAmount == 0) break;
-
-                    //Can we find what we're looking for?
-                    var partList = v.Parts.Where(
-                        p => p.Resources.Contains(name)
-                        && p != part
-                        && p.protoModuleCrew.Count > 0);
-                    foreach (var p in partList)
-                    {
-                        var pr = p.Resources[name];
-                        var storageSpace = pr.maxAmount - pr.amount;
-
-                        if (storageSpace >= transferAmount)
-                        {
-                            // SS: 100
-                            // RemotePartAmount:        400/500 -> 450/500
-                            // LocalPartAmount:         100     -> 50
-                            // TransferAmount:          50      -> 0
-                            pr.amount += transferAmount;
-                            part.Resources[name].amount -= transferAmount;
-                            transferAmount = 0;
-                            break;
-                        }
-                        else
-                        {
-                            // SS:10
-                            // RemotePartAmount:        490/500 -> 500/500
-                            // LocalPartAmount:         100     -> 90
-                            // TransferAmount:          50      -> 40
-                            transferAmount -= storageSpace;
-                            part.Resources[name].amount -= storageSpace;
-                            pr.amount = pr.maxAmount;
-                        }
-                    }
-                }
-                return transferAmount;
-            }
-            catch (Exception ex)
-            {
-                print(String.Format("[MKS] - ERROR in TransferLifeSupport - {0}",ex.Message));
-                throw;
-            }
-        }
-
-        private enum TransferType
-        {
-            TakeResources,
-            StoreResources
-        }
-
-        private List<Vessel> GetNearbyVessels()
-        {
-            try
-            { 
-                var vessels = new List<Vessel>();
-                foreach (var v in FlightGlobals.Vessels.Where(
-                    x => x.mainBody == vessel.mainBody 
-                    && x.Landed))
-                {
-                    var posCur = vessel.GetWorldPos3D();
-                    var posNext = v.GetWorldPos3D();
-                    var distance = Vector3d.Distance(posCur, posNext);
-                    if (distance < LogisticsRange)
-                    {
-                        vessels.Add(v);
-                    }
-                }
-                return vessels;
-            }
-            catch (Exception ex)
-            {
-                print(String.Format("[MKS] - ERROR in GetNearbyVessels - {0}", ex.Message));
-                throw;
-            }
-        }
-
-        private void TransferResources(PartResourceDefinition resource, double amount, TransferType transferType)
-        {
-            try
-            {
-                var transferAmount = amount;
-                var nearVessels = GetNearbyVessels();
-
-                foreach (var v in nearVessels)
-                {
-                    if (transferAmount == 0) break;
-
-                    //Can we find what we're looking for?
-                    var partList = v.Parts.Where(
-                        p => p.Resources.Contains(resource.name) 
-                        && p!= part);
-                    foreach(var p in partList)
-                    {
-                        var pr = p.Resources[resource.name];
-                        if (transferType == TransferType.TakeResources)
-                        {
-                            // RemotePartAmount:       200    -> 150
-                            // LocalPartAmount:         10    -> 60
-                            // TransferAmount:          50    -> 0
-                            //  
-                            if (pr.amount >= transferAmount)
-                            {
-                                pr.amount -= transferAmount;
-                                part.Resources[resource.name].amount += transferAmount;
-                                transferAmount = 0;
-                                break;
-                            }
-                            else
-                            {
-                                // RemotePartAmount:        10    -> 0
-                                // LocalPartAmount:         10    -> 20
-                                // TransferAmount:          50    -> 40
-                                // 
-                                transferAmount -= pr.amount;
-                                part.Resources[resource.name].amount += pr.amount;
-                                pr.amount = 0;
-                            }
-                        }
-                        else
-                        {
-                            var storageSpace = pr.maxAmount - pr.amount;
-
-                            if (storageSpace >= transferAmount)
-                            {
-                                // SS: 100
-                                // RemotePartAmount:        400/500 -> 450/500
-                                // LocalPartAmount:         100     -> 50
-                                // TransferAmount:          50      -> 0
-                                pr.amount += transferAmount;
-                                part.Resources[resource.name].amount -= transferAmount;
-                                transferAmount = 0;
-                                break;
-                            }
-                            else
-                            {
-                                // SS:10
-                                // RemotePartAmount:        490/500 -> 500/500
-                                // LocalPartAmount:         100     -> 90
-                                // TransferAmount:          50      -> 40
-                                transferAmount -= storageSpace;
-                                part.Resources[resource.name].amount -= storageSpace;
-                                pr.amount = pr.maxAmount;
-                            }
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                print(String.Format("[MKS] - ERROR in TransferResources - {0}", ex.Message));
-                throw;
-            }
-        }
 
         public override void OnAwake()
         {
@@ -370,8 +135,7 @@ namespace KolonyTools
                     if (eff > 2) eff = 2;
                     if (eff < .25) eff = .1f;
                 }
-                efficiency = String.Format("{0}%", Math.Round((eff * 100),1));
-                efficiencyStats = String.Format("ker:{0} spc:{1} mod:{2}", Math.Round(numKerbals,1), numWorkspaces, numModules);
+                efficiency = String.Format("{0}% [{1}k/{2}s/{3}m", Math.Round((eff * 100),1),Math.Round(numKerbals,1), numWorkspaces, numModules);
                 return eff;
             }
             catch (Exception ex)
---
+++ Source/KolonyTools/ProxyLogistics.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KolonyTools
+{
+    public class LogisticsGoal
+    {
+        public PartResourceDefinition Resource { get; set; }
+        public double Goal { get; set; }
+    }
+    public class ProxyLogistics : PartModule
+    {
+        [KSPField]
+        public int LogisticsRange = 100; 
+
+        [KSPField]
+        public string LogisticsResources = "";
+
+        [KSPField]
+        public bool IsLogisticsDistributor = false;
+
+        private List<LogisticsGoal> ResourceList;
+        private List<string> PrecisionResources = new List<string> { "EnrichedSoil","ConstructionParts"};
+        private static char[] delimiters = { ' ', ',', '\t', ';' };
+        private int DistributionRange;
+
+        public override void OnLoad(ConfigNode node)
+        {
+            ResourceList = SetupResourceList(LogisticsResources);
+            base.OnLoad(node);
+        }
+
+        public override void OnAwake()
+        {
+            ResourceList = SetupResourceList(LogisticsResources);
+            base.OnAwake();
+        }
+
+        private List<LogisticsGoal> SetupResourceList(string resString)
+        {
+            try
+            {
+                //Configured Resources
+                var resources = new List<LogisticsGoal>();
+                string[] tokens = resString.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+                for (int i = 0; i < (tokens.Length - 1); i += 2)
+                {
+                    PartResourceDefinition resource = PartResourceLibrary.Instance.GetDefinition(tokens[i]);
+                    double goal;
+                    if (resource != null && double.TryParse(tokens[i + 1], out goal))
+                    {
+                        resources.Add(new LogisticsGoal {Resource= resource, Goal = goal});
+                    }
+                    else
+                    {
+                        print("[MKS] Cannot parse \"" + resString + "\", something went wrong.");
+                    }
+                }
+                //Crew Resources
+                if(part.CrewCapacity > 0)
+                {
+                    PartResourceDefinition food = PartResourceLibrary.Instance.GetDefinition("Food");
+                    PartResourceDefinition water = PartResourceLibrary.Instance.GetDefinition("Water");
+                    PartResourceDefinition oxygen = PartResourceLibrary.Instance.GetDefinition("Oxygen");
+
+                    PrecisionResources.Add("Food");
+                    PrecisionResources.Add("Water");
+                    PrecisionResources.Add("Oxygen");
+
+                    if (!resources.Where(r => r.Resource.name == "Food").Any())
+                        resources.Add(new LogisticsGoal { Resource = food, Goal = part.CrewCapacity });
+                    if(!resources.Where(r=>r.Resource.name == "Water").Any())
+                        resources.Add(new LogisticsGoal { Resource = water, Goal = part.CrewCapacity });
+                    if (!resources.Where(r => r.Resource.name == "Oxygen").Any())
+                        resources.Add(new LogisticsGoal { Resource = oxygen, Goal = part.CrewCapacity });
+                }
+                //ElectricCharge
+                if(part.Resources.Contains("ElectricCharge"))
+                {
+                    PartResourceDefinition electricCharge = PartResourceLibrary.Instance.GetDefinition("ElectricCharge");
+                    var ec = part.Resources["ElectricCharge"];
+                    if (!resources.Where(r => r.Resource.name == "ElectricCharge").Any())
+                        resources.Add(new LogisticsGoal { Resource = electricCharge, Goal = ec.maxAmount });
+                }
+                return resources;
+            }
+            catch (Exception ex)
+            {
+                print(String.Format("[MKS] - ERROR in UpdateResourceList - {0}", ex.Message));
+                throw;
+            }
+        }
+        public override void OnFixedUpdate()
+        {
+            GetLogisticsRange();
+            if (DistributionRange > 0)
+            {
+                CheckResources();
+            }
+            base.OnFixedUpdate();
+        }
+
+        private void GetLogisticsRange()
+        {
+            var vlist = GetNearbyVessels(LogisticsRange);
+            foreach(var v in vlist)
+            {
+                var partList = v.Parts.Where(
+                    p => p.Modules.Contains("ProxyLogistics")
+                    && p != part);
+                foreach (var p in partList)
+                {
+                    var mod = (ProxyLogistics)p.Modules["ProxyLogistics"];
+                    if (mod.IsLogisticsDistributor && mod.LogisticsRange > DistributionRange)
+                    {
+                        DistributionRange = mod.LogisticsRange;
+                        print("[MKS] Setting DistributionRange to " + DistributionRange);
+                    }
+                }
+            }
+        }
+
+        private List<Vessel> GetNearbyVessels(int range)
+        {
+            try
+            {
+                var vessels = new List<Vessel>();
+                foreach (var v in FlightGlobals.Vessels.Where(
+                    x => x.mainBody == vessel.mainBody
+                    && x.Landed
+                    && x != vessel))
+                {
+                    var posCur = vessel.GetWorldPos3D();
+                    var posNext = v.GetWorldPos3D();
+                    var distance = Vector3d.Distance(posCur, posNext);
+                    if (distance < range)
+                    {
+                        vessels.Add(v);
+                    }
+                }
+                return vessels;
+            }
+            catch (Exception ex)
+            {
+                print(String.Format("[MKS] - ERROR in GetNearbyVessels - {0}", ex.Message));
+                throw;
+            }
+        }
+
+        private void CheckResources()
+        {
+            try
+            {
+                print("***Starting Loop...");
+                foreach(var r in ResourceList)
+                {
+                    print("Getting Part " + r.Resource.name + " for " + part.name);
+                    if (part.Resources.Contains(r.Resource.name))
+                    {
+                        var pr = part.Resources[r.Resource.name];
+                        var threshold = Math.Round(pr.maxAmount * .1);
+                        if (threshold < 1) threshold = 1;
+                        var transferAmount = threshold;
+                        print("Checking Precision Resources");
+                        if (PrecisionResources.Contains(r.Resource.name)) threshold = 0; //Odds are this is for constructionParts or soil
+                        //  CAP     GOAL    THRESH  AMOUNT
+                        //  100     50      10      35  TAKE 10
+                        //  100     50      10      45  NO ACTION
+                        //  100     50      10      65  GIVE 10
+                        if ((pr.amount + threshold) < r.Goal) //Take
+                        {
+                            if (transferAmount > (pr.maxAmount - pr.amount)) transferAmount = pr.maxAmount - pr.amount;
+                            print("Transferring In " + transferAmount + " " + r.Resource.name);
+                            TransferResources(r.Resource, transferAmount, TransferType.TakeResources);
+                        }
+                        else if ((pr.amount - threshold) > r.Goal) //Give
+                        {
+                            if (transferAmount > pr.amount) transferAmount = pr.amount;
+                            print("Transferring Out " + transferAmount + " " + r.Resource.name);
+                            TransferResources(r.Resource, transferAmount, TransferType.StoreResources);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                print(String.Format("[MKS] - ERROR in CheckResources - {0}", ex.StackTrace));
+                throw;
+            }
+        }
+
+        private void TransferResources(PartResourceDefinition resource, double amount, TransferType transferType)
+        {
+            try
+            {
+                var transferAmount = amount;
+                var nearVessels = GetNearbyVessels(DistributionRange);
+                foreach (var v in nearVessels)
+                {
+                    if (transferAmount == 0) break;
+                    //Can we find what we're looking for?
+                    var partList = v.Parts.Where(
+                        p => p.Resources.Contains(resource.name)
+                        && p != part);
+                    foreach (var p in partList)
+                    {
+                        var pr = p.Resources[resource.name];
+                        if (transferType == TransferType.TakeResources)
+                        {
+                            // RemotePartAmount:       200    -> 150
+                            // LocalPartAmount:         10    -> 60
+                            // TransferAmount:          50    -> 0
+                            //  
+                            if (pr.amount >= transferAmount)
+                            {
+                                pr.amount -= transferAmount;
+                                part.Resources[resource.name].amount += transferAmount;
+                                transferAmount = 0;
+                                break;
+                            }
+                            else
+                            {
+                                // RemotePartAmount:        10    -> 0
+                                // LocalPartAmount:         10    -> 20
+                                // TransferAmount:          50    -> 40
+                                // 
+                                transferAmount -= pr.amount;
+                                part.Resources[resource.name].amount += pr.amount;
+                                pr.amount = 0;
+                            }
+                        }
+                        else
+                        {
+                            var storageSpace = pr.maxAmount - pr.amount;
+                            if (storageSpace >= transferAmount)
+                            {
+                                // SS: 100
+                                // RemotePartAmount:        400/500 -> 450/500
+                                // LocalPartAmount:         100     -> 50
+                                // TransferAmount:          50      -> 0
+                                pr.amount += transferAmount;
+                                part.Resources[resource.name].amount -= transferAmount;
+                                transferAmount = 0;
+                                break;
+                            }
+                            else
+                            {
+                                // SS:10
+                                // RemotePartAmount:        490/500 -> 500/500
+                                // LocalPartAmount:         100     -> 90
+                                // TransferAmount:          50      -> 40
+                                transferAmount -= storageSpace;
+                                part.Resources[resource.name].amount -= storageSpace;
+                                pr.amount = pr.maxAmount;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                print(String.Format("[MKS] - ERROR in TransferResources - {0}", ex.StackTrace));
+                throw;
+            }
+        }
+
+        private enum TransferType
+        {
+            TakeResources,
+            StoreResources
+        }
+
+    }
+}
