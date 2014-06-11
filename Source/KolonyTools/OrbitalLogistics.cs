e7a15685afbe12482975fa108df96deaf3fbc177
Bob Palmer <rbpalmer2222@gmail.com>
2014-06-10 21:10:51 -0400

    ORS Support

--- Source/KolonyTools/OrbitalLogistics.cs
+++ Source/KolonyTools/OrbitalLogistics.cs
@@ -6,8 +6,32 @@ using UnityEngine;
 
 namespace KolonyTools 
 {
-    public class OrbitalLogistics : PartModule
+    public class OrbitalLogisticsHub : PartModule
     {
+        private Rect _windowPosition = new Rect();
 
+        public override void OnStart(StartState state)
+        {
+            if (state != StartState.Editor)
+            {
+                RenderingManager.AddToPostDrawQueue(0,OnDraw);
+            }
+        }
+
+        private void OnDraw()
+        {
+            if (vessel == FlightGlobals.ActiveVessel)
+            {
+                _windowPosition= GUILayout.Window(66,_windowPosition,OnWindow,"This is a title");
+            }
+        }
+
+        private void OnWindow(int windowId)
+        {
+            GUILayout.BeginHorizontal(GUILayout.Width(250f));
+            GUILayout.Label("This is a label");
+            GUILayout.EndHorizontal();
+            GUI.DragWindow();
+        }
     }
 }
