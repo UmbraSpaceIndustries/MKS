5d10e6eb076ce4a337369569a54aa6c8ebd98c9b
Bob Palmer <rbpalmer_nrt@hotmail.com>
2017-01-07 10:50:18 -0500

    2017.01.07 Release

---
+++ Source/KolonyTools/KolonyTools/Experience/ExplorerSkill.cs
@@ -0,0 +1,25 @@
+﻿using Experience;
+
+namespace KolonyTools.Experience
+{
+    public class ExplorerSkill : ExperienceEffect
+    {
+        public ExplorerSkill(ExperienceTrait parent) : base(parent)
+        {
+        }
+
+        public ExplorerSkill(ExperienceTrait parent, float[] modifiers) : base(parent, modifiers)
+        {
+        }
+
+        protected override float GetDefaultValue()
+        {
+            return 0f;
+        }
+
+        protected override string GetDescription()
+        {
+            return "Ability to function even when far from home in adverse conditions";
+        }
+    }
+}
