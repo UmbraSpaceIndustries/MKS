87ef9845453d86a93a5d3b4c62c0f172314f788a
doktorkrogg <doktorkrogg@users.noreply.github.com>
2018-10-15 18:12:41 -0400

    Bug fix: GUI NREs in OrbLog
    OrbLog was relying on vessel ids to keep track of transfers and would throw a null ref when trying to list pending and expired transfers if the vessel no longer existed; modified the code to cache vessel names in the transfer request; added a unique identifier to ModuleOrbitalLogistics as a backup method of tracking down a vessel if its vessel id changes (e.g. after docking/undocking)

--- Source/KolonyTools/KolonyTools/OrbitalLogistics/OrbitalLogisticsTransferRequest.cs
+++ Source/KolonyTools/KolonyTools/OrbitalLogistics/OrbitalLogisticsTransferRequest.cs
@@ -42,6 +42,24 @@ namespace KolonyTools
         [Persistent(name = "OriginVesselId")]
         protected string _originId;
 
+        // Whenever a vessel is docked, it basically disappears from the game AND loses
+        //  its vessel id. When it is undocked, it receives a new vessel id. So we cache
+        //  the vessel names in the transfer request when it is created in order to avoid
+        //  UI issues when trying to find vessel names for *missing* vessels.
+        [Persistent(name = "DestinationVesselName")]
+        protected string _destinationName;
+
+        [Persistent(name = "OriginVesselName")]
+        protected string _originName;
+
+        // To help locate a vessel if it is a victim of vessel id reassignment surgery,
+        //  we'll cache the module id of the ModuleOrbitalLogistics that initiated the transfer.
+        [Persistent(name = "DestinationModuleId")]
+        protected string _destinationModuleId;
+
+        [Persistent(name = "OriginModuleId")]
+        protected string _originModuleId;
+
         [Persistent(name = "Mass")]
         protected float _mass;
 
@@ -70,6 +88,7 @@ namespace KolonyTools
         /// <summary>
         /// The <see cref="Vessel"/> resources will be deducted from.
         /// </summary>
+        /// <remarks>Use the <see cref="OriginVesselName"/> property for UI display purposes.</remarks>
         public Vessel Origin
         {
             get
@@ -79,6 +98,15 @@ namespace KolonyTools
                     _origin = FlightGlobals.Vessels
                         .Where(v => v.id.ToString() == _originId)
                         .SingleOrDefault();
+
+                    // If the origin disappeared, try to find it by its OrbLog module instead.
+                    if (_origin == null)
+                    {
+                        _origin = FlightGlobals.Vessels
+                            .Where(v => v.FindPartModulesImplementing<ModuleOrbitalLogistics>()
+                                .Any(m => m.ModuleId == _originModuleId))
+                            .FirstOrDefault();
+                    }
                 }
 
                 return _origin;
@@ -87,12 +115,15 @@ namespace KolonyTools
             {
                 _origin = value;
                 _originId = value.id.ToString();
+                _originName = value.vesselName;
+                _originModuleId = ModuleOrbitalLogistics.GetOrbLogModuleIdForVessel(value);
             }
         }
 
         /// <summary>
         /// The <see cref="Vessel"/> resources will be added to.
         /// </summary>
+        /// <remarks>Use the <see cref="DestinationVesselName"/> property for UI display purposes.</remarks>
         public Vessel Destination
         {
             get
@@ -104,12 +135,45 @@ namespace KolonyTools
                         .SingleOrDefault();
                 }
 
+                // If the destination disappeared, try to find it by its OrbLog module instead.
+                if (_destination == null)
+                {
+                    _destination = FlightGlobals.Vessels
+                        .Where(v => v.FindPartModulesImplementing<ModuleOrbitalLogistics>()
+                            .Any(m => m.ModuleId == _originModuleId))
+                        .FirstOrDefault();
+                }
+
                 return _destination;
             }
             set
             {
                 _destination = value;
                 _destinationId = value.id.ToString();
+                _destinationName = value.vesselName;
+                _destinationModuleId = ModuleOrbitalLogistics.GetOrbLogModuleIdForVessel(value);
+            }
+        }
+
+        /// <summary>
+        /// The name of the <see cref="Vessel"/> resources will be added to. Use this for UI display purposes.
+        /// </summary>
+        public string DestinationVesselName
+        {
+            get
+            {
+                return Destination == null ? _destinationName : Destination.vesselName;
+            }
+        }
+
+        /// <summary>
+        /// The name of the <see cref="Vessel"/> resources will be removed from. Use this for UI display purposes.
+        /// </summary>
+        public string OriginVesselName
+        {
+            get
+            {
+                return Origin == null ? _originName : Origin.vesselName;
             }
         }
         #endregion
